87797df33c6b78f5fc279829b4b30bb76de12c83
Gregory Pacheco <gpzim98@gmail.com>
2018-04-22 13:30:59 +0100

    Filters and search

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from atracoes.models import Atracao
 from .serializers import AtracaoSerializer
@@ -6,3 +7,5 @@ from .serializers import AtracaoSerializer
 class AtracaoViewSet(ModelViewSet):
     queryset = Atracao.objects.all()
     serializer_class = AtracaoSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('nome', 'descricao')
--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,5 +1,5 @@
 from rest_framework.decorators import action
-from rest_framework.response import Response
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -10,9 +10,25 @@ class PontoTuristicoViewSet(ModelViewSet):
     A simple ViewSet for viewing and editing accounts.
     """
     serializer_class = PontoTuristicoSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('nome', 'descricao', 'endereco__linha1')
 
     def get_queryset(self):
-        return PontoTuristico.objects.filter(aprovado=True)
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+        queryset = PontoTuristico.objects.all()
+
+        if id:
+            queryset = PontoTuristico.objects.filter(pk=id)
+
+        if nome:
+            queryset = queryset.filter(nome__iexact=nome)
+
+        if descricao:
+            queryset = queryset.filter(descricao__iexact=descricao)
+
+        return queryset
 
     def list(self, request, *args, **kwargs):
         return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
