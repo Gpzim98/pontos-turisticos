1010b5b68488255558f6b781484118d7996df5bb
Gregory Pacheco <gpzim98@gmail.com>
2018-06-04 14:00:08 +0100

    Simulando nova versao

--- atracoes/models.py
+++ atracoes/models.py
@@ -7,6 +7,7 @@ class Atracao(models.Model):
     horario_func = models.TextField()
     idade_minima = models.IntegerField()
     foto = models.ImageField(upload_to='atracoes', null=True, blank=True)
+    observacoes = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.nome
